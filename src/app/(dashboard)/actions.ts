2134d52b04b23018afae7fd1bfaffee369be3f20
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-01 19:03:23 -0300

    refactor: add get-products API endpoint and update deleteProduct function

--- src/app/(dashboard)/actions.ts
+++ src/app/(dashboard)/actions.ts
@@ -1,10 +1,10 @@
 'use server'
 
-import { deleteProductById } from '../../../pages/api/products/route'
 import { revalidatePath } from 'next/cache'
 
 export async function deleteProduct(formData: FormData) {
   const id = Number(formData.get('id'))
-  await deleteProductById(id)
+  return id
+  // await deleteProductById(id)
   revalidatePath('/')
 }
