b3fd7abf30ebc957f3e535cf50e1add567735068
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-01 18:26:30 -0300

    feat: add Product-item and Products-table components

---
+++ src/app/(dashboard)/Product-item.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { MoreHorizontal } from 'lucide-react'
+import { TableCell, TableRow } from '@/components/ui/table'
+import { Product } from '@prisma/client'
+import { deleteProduct } from './actions'
+
+export function ProductItem({ product }: { product: Product }) {
+  return (
+    <TableRow>
+      <TableCell className="hidden sm:table-cell">
+        <Image
+          alt="Product image"
+          className="aspect-square rounded-md object-cover"
+          height="64"
+          src={product.imagePath}
+          width="64"
+        />
+      </TableCell>
+      <TableCell className="font-medium">{product.name}</TableCell>
+      <TableCell>
+        <Badge variant="outline" className="capitalize">
+          {product.status}
+        </Badge>
+      </TableCell>
+      <TableCell className="hidden md:table-cell">{`$${product.retailPrice}`}</TableCell>
+      <TableCell className="hidden md:table-cell">
+        {product.minQuantity}
+      </TableCell>
+      <TableCell className="hidden md:table-cell">{product.status}</TableCell>
+      <TableCell>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button aria-haspopup="true" size="icon" variant="ghost">
+              <MoreHorizontal className="h-4 w-4" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>
+              <form action={deleteProduct}>
+                <button type="submit">Delete</button>
+              </form>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </TableCell>
+    </TableRow>
+  )
+}
