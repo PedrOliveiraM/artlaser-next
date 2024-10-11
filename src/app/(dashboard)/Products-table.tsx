94be954e88f129882baf70cbe5b82193935ddf45
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-11 20:08:26 -0300

    feat: ensuring pagination through the selection of the number of items

--- src/app/(dashboard)/Products-table.tsx
+++ src/app/(dashboard)/Products-table.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Card,
   CardContent,
@@ -12,11 +13,21 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
 import { Product } from '@prisma/client'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import TemplateCardFooter from './CardFooter'
 import { ProductItem } from './Product-item'
+import { Button } from '@/components/ui/button'
 
 export function ProductsTable({
   products,
@@ -29,58 +40,85 @@ export function ProductsTable({
 }) {
   const router = useRouter()
   const searchParams = useSearchParams()
-  const productsPerPage = 2
+  const [itemsPerPage, setItemsPerPage] = useState(2)
 
-  const minOffset = 1
-  const maxOffset = () => {
-    if (totalProducts % productsPerPage === 0) {
-      return totalProducts / productsPerPage
-    } else {
-      return Math.floor(totalProducts / productsPerPage) + 1
-    }
+  const handleSelectChange = (value: string) => {
+    setItemsPerPage(Number(value))
+    setOffset(1) // Reseta para a primeira página quando os itens por página mudam
   }
 
+  const minOffset = 1
+  const pages = () => {
+    return Math.ceil(totalProducts / itemsPerPage) || 1 // Para garantir pelo menos uma página
+  }
+
+  const [maxOffset, setMaxOffset] = useState(pages())
+
+  useEffect(() => {
+    setMaxOffset(pages())
+  }, [itemsPerPage, totalProducts]) // Atualiza quando totalProducts ou itemsPerPage muda
+
   function validOffset(offset: number): boolean {
-    return offset > 0 && offset <= maxOffset()
+    return offset > 0 && offset <= maxOffset
   }
 
   const initialOffset = () => {
     const offsetParams = Number(searchParams?.get('offset'))
-
-    if (validOffset(offsetParams)) return offsetParams
-    else return minOffset
+    return validOffset(offsetParams) ? offsetParams : minOffset
   }
 
   const [offset, setOffset] = useState(initialOffset)
-
-  function nextPage() {
-    const newOffset = offset + 1
-    setOffset(newOffset)
-    router.push(`/?offset=${newOffset}`, { scroll: false })
-  }
-
-  function prevPage() {
-    const newOffset = Math.max(1, offset - productsPerPage)
-    setOffset(newOffset)
-    router.push(`/?offset=${newOffset}`, { scroll: false })
-  }
 
   const paginatedProducts = useMemo(() => {
     const filtered = products.filter((product) =>
       product.name.toLowerCase().includes(filter?.toLowerCase() || ''),
     )
+    return filtered.slice((offset - 1) * itemsPerPage, offset * itemsPerPage)
+  }, [products, filter, offset, itemsPerPage]) // Adicione itemsPerPage aqui
 
-    return filtered.slice(
-      (offset - 1) * productsPerPage,
-      offset * productsPerPage,
-    )
-  }, [products, filter, offset])
+  useEffect(() => {
+    setOffset(1) // Reseta o offset quando a lista de produtos mudar
+  }, [products])
+
+  function nextPage() {
+    const newOffset = offset + 1
+    if (validOffset(newOffset)) {
+      setOffset(newOffset)
+      router.push(`/?offset=${newOffset}`, { scroll: false })
+    }
+  }
+
+  function prevPage() {
+    const newOffset = Math.max(1, offset - 1)
+    if (validOffset(newOffset)) {
+      setOffset(newOffset)
+      router.push(`/?offset=${newOffset}`, { scroll: false })
+    }
+  }
 
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Produtos</CardTitle>
-        <CardDescription>Gerencie aqui seus produtos</CardDescription>
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <CardTitle>Produtos</CardTitle>
+            <CardDescription>Gerencie aqui seus produtos</CardDescription>
+          </div>
+          <div className="flex gap-2">
+            <Button>Cadastrar</Button>
+            <Select onValueChange={handleSelectChange}>
+              <SelectTrigger className="w-20">
+                <SelectValue placeholder="Itens" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="5">5</SelectItem>
+                <SelectItem value="10">10</SelectItem>
+                <SelectItem value="20">20</SelectItem>
+                <SelectItem value="30">30</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
       </CardHeader>
 
       <CardContent>
@@ -90,23 +128,17 @@ export function ProductsTable({
               <TableHead className="hidden w-[100px] text-center sm:table-cell">
                 Image
               </TableHead>
-
               <TableHead className="text-center">Name</TableHead>
-
               <TableHead className="text-center">Status</TableHead>
-
               <TableHead className="hidden text-center md:table-cell">
                 Preço de varejo
               </TableHead>
-
               <TableHead className="hidden text-center md:table-cell">
                 Preço de atacado
               </TableHead>
-
               <TableHead className="hidden text-center md:table-cell">
                 Categoria
               </TableHead>
-
               <TableHead>
                 <span className="sr-only">Actions</span>
               </TableHead>
@@ -124,8 +156,8 @@ export function ProductsTable({
         prevPage={prevPage}
         nextPage={nextPage}
         totalItems={totalProducts}
-        itemsPerPage={productsPerPage}
-        maxOffset={maxOffset()} // Aqui você chama a função maxOffset
+        itemsPerPage={itemsPerPage}
+        maxOffset={maxOffset}
       />
     </Card>
   )
