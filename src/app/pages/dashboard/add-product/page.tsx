f7f4daaccc67c620161fd1cf6d43cb38bbc944ea
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-15 18:40:47 -0300

    chore: updated setup

--- src/app/pages/dashboard/add-product/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductForm from '@/app/components/ProductForm'
-
-export default function Addproduct() {
-  return (
-    <div className="flex w-full justify-center bg-zinc-300">
-      <ProductForm />
-    </div>
-  )
-}
