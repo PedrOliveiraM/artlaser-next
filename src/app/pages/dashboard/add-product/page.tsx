8c56a8fa1557ed96e9f2a62a604c6a89fe679693
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-15 13:00:06 -0300

    chore: new page add-product

---
+++ src/app/pages/dashboard/add-product/page.tsx
@@ -0,0 +1,9 @@
+import ProductForm from '@/components/ProductForm'
+
+export default function Addproduct() {
+  return (
+    <div className="w-96 bg-zinc-300">
+      <ProductForm />
+    </div>
+  )
+}
