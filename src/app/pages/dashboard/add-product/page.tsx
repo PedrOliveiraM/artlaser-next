528a74c252c54f62238414ac3dc5f45f69020b77
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-15 16:01:46 -0300

    style: center form

--- src/app/pages/dashboard/add-product/page.tsx
+++ src/app/pages/dashboard/add-product/page.tsx
@@ -2,7 +2,7 @@ import ProductForm from '@/components/ProductForm'
 
 export default function Addproduct() {
   return (
-    <div className="w-96 bg-zinc-300">
+    <div className="flex w-full justify-center bg-zinc-300">
       <ProductForm />
     </div>
   )
