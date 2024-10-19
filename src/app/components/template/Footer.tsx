95d0ffd90a50d268ac63a9c2f8099082eb1b7c47
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-19 15:05:11 -0300

    chore: config shadcn setup with yours components

--- src/app/components/template/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer className="bottom-0 m-auto p-5">
-      <p>© 2024 Artlaser. Todos os direitos reservados</p>
-    </footer>
-  )
-}
